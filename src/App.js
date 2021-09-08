ef3e2b2c79e4e87dfaeb84cd534bedb755a81e23
Mark S <marksherm04@gmail.com>
2021-09-08 18:36:09 -0400

    feature/navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
 import Preferences from './components/Preferences/Preferences';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
-import Logout from './components/';
 
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+  
   return (
     <div className="wrapper">
-      <h1>Application</h1>
+      <h1>Hot Java</h1>
       <BrowserRouter>
         <Switch>
           <Route path="/dashboard">
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import React from 'react';
-
 export default function Dashboard() {
 	return(
 		<h2>Dashboard</h2>
