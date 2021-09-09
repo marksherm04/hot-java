4f70869df0bad85056e5c42bab8bf1be0177b432
Mark S <marksherm04@gmail.com>
2021-09-08 20:36:14 -0400

    fixed errors

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -30,14 +30,14 @@ export default function Login({ setToken }) {
 	return (
 		<div className="login-wrapper">
 			<h1>Login</h1>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label>
 					<p>Username</p>
 					<input type="text" onChange={e => setUserName(e.target.value)}/>
 				</label>
 				<label>
 					<p>Password</p>
-					<input type="password" onChange={e => setUserName(e.target.value)}/>
+					<input type="password" onChange={e => setPassword(e.target.value)}/>
 				</label>
 				<div>
 					<button type="submit">Submit</button>
