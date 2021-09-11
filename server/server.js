6c2d08302f9d387a121731b6034accaa689605c6
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-09-11 19:18:00 -0400

    added node packages and updates to server files

--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(cors());
 
-app.use('/login', (req, res) => {
-  res.send({
-    token: 'test123'
+// app.use('/login', (req, res) => {
+//   res.send({
+//     token: 'test123'
+//   });
+// });
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
-
-app.listen(3000, () => console.log('API is running on http://localhost:3000/login'));
