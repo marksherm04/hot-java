0511eafeba82028427203f08d7895f7673a25bca
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-09-11 20:01:19 -0400

    updated typedefs and resolvers for user, post, and comment models

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,41 @@
-const resolvers = {
+const { User, Post } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+// const { signToken } = require('../utils/auth);
 
+const resolvers = {
+  Query: {
+    me: async (parents, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('posts');
+        
+        return userData; 
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+    // get all posts
+
+    // get single post by id
+
+    // get all users
+
+    // get user by username
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // login
+
+    // add post
+
+    // add comment
+  }
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,48 @@
 const { gql } = require ('apollo-server-express');
 
 const typeDefs = gql`
+  type Post {
+    _id: ID
+    postText: string
+    createdAt: String
+    username: String
+    commentCount: Int
+    comments: [Comment]
+  }
 
+  type Comment {
+    _id: ID
+    commentBody: String
+    createdAt: String
+    username: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    postCount: Int
+    comments: [Comment]
+  }
+
+  type Query {
+    me: User
+    user: [User]
+    user:(username: String!): User
+    posts(username: String): [Post]
+    comment(_id: ID!): Comment
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    addPost(postText: String!): Post
+    addComment(postId: ID!, commentBody: String!): Post
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
