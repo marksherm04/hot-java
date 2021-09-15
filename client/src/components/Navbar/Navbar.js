f1a789727788a2531a1e9485f17294cf1cbd3abb
Mark S <marksherm04@gmail.com>
2021-09-15 16:29:20 -0400

    updated css, changing color scheme

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
+import hotJavaDark from './hotJavaDark.jpg';
+
 
 const Navbar = () => {
 	const logout = event => {
@@ -11,25 +13,26 @@ const Navbar = () => {
 	return (
 		<header className="bg-secondary mb-4 py-2 flex-row align-center">
 			<div className="container flex-row justify-space-between-lg justify-center align-center">
-				
+
 				<Link to="/">
-				<h1>Hot Java</h1>
+					<div className="logo">
+						<img src={hotJavaDark} alt="Hot Java Logo" />
+					</div>
 				</Link>
-			
 				<nav className="text-center">
-			{Auth.loggedIn()?(
-				<>
-				<Link to ="/profile">My Bevs</Link>
-				<a href="/" onClick={logout}>
-				Logout
-				</a>
-				</>
-			): (
-				<>
-				<Link to="/login">Login</Link>
-				<Link to="/signup">Signup</Link>
-				</>
-			)}
+					{Auth.loggedIn() ? (
+						<>
+							<Link to="/profile">My Bevs</Link>
+							<a href="/" onClick={logout}>
+								Logout
+							</a>
+						</>
+					) : (
+						<>
+							<Link to="/login">Login</Link>
+							<Link to="/signup">Signup</Link>
+						</>
+					)}
 				</nav>
 			</div>
 		</header>
